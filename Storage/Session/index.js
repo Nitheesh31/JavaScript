ebc7381a4f03fc300e4cb6deeaba0aad2a751426
Nitheesh A <nitheeshgeethun@gmail.com>
2026-04-08 18:31:24 +0530

    Update index.js

--- Storage/Session/index.js
+++ Storage/Session/index.js
@@ -1,8 +1,8 @@
 sessionStorage.setItem('name',"sudesh")
 sessionStorage.setItem('age',25)
 
-// sessionStorage.clear()
-// sessionStorage.removeItem('age')
+sessionStorage.clear()
+sessionStorage.removeItem('age')
 
 let name=sessionStorage.getItem('name')
 let age=sessionStorage.getItem('age')
