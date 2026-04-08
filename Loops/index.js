c235813c85d580394dc6f349df3afc218defe326
Nitheesh A <nitheeshgeethun@gmail.com>
2026-04-08 18:19:47 +0530

    Update index.js

--- Loops/index.js
+++ Loops/index.js
@@ -2,16 +2,16 @@ let i=0;
 let n=5;
 
 // //While loop
-// while(i<n){ //condition check staring
-//     console.log(i);
-//     i++;
-// }
+while(i<n){ //condition check staring
+    console.log(i);
+    i++;
+}
 
 // //do while loop
-// do{
-//     console.log(i);
-//     i++;
-// }while(i<=n); //condition check ending
+do{
+    console.log(i);
+    i++;
+}while(i<=n); 
 
 //for loop
 for(let i=0;i<50;i++){
